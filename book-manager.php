f6e945a57720043170687f3c208b74822375b817
Amir Safari <amir1.safari@gmail.com>
2025-12-09 11:50:29 +0330

    Add Book Manager plugin
    Introduced the initial Book Manager WordPress plugin with basic setup and autoloading via Composer.

---
+++ book-manager.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:     Book Manager
+ * Plugin URI:      https://example.com
+ * Plugin Prefix:   BOMAN
+ * Description:     Manage book information with custom table and post types.
+ * Author:          Amir Safari
+ * Author URI:      https://profiles.wordpress.org/amirsafaridevs/
+ * Text Domain:     book-manager
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+    require dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+
+
+
