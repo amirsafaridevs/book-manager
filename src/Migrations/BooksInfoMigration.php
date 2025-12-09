b18a656965176bb0220ee9ff253879253ddf61e4
Amir Safari <amir1.safari@gmail.com>
2025-12-09 13:35:20 +0330

    Add BooksInfoMigration class stub
    Introduces the BooksInfoMigration class implementing MigrationInterface with placeholder up and down methods for future migration logic.

---
+++ src/Migrations/BooksInfoMigration.php
@@ -0,0 +1,16 @@
+<?php
+namespace BookManager\Migrations;
+
+use BookManager\Contracts\MigrationInterface;
+
+class BooksInfoMigration implements MigrationInterface
+{
+    public function up()
+    {
+        // TODO: Implement up() method.
+    }
+    public function down()
+    {
+        // TODO: Implement down() method.
+    }
+}
