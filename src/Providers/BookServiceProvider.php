df958a7e514090ae93f17f3f82ccef3db718958e
Amir Safari <amir1.safari@gmail.com>
2025-12-09 19:12:51 +0330

    Add Book custom post type service
    Introduces BookPostTypeService to register a 'book' custom post type in WordPress. Updates BookServiceProvider to register and initialize the service during plugin boot.

--- src/Providers/BookServiceProvider.php
+++ src/Providers/BookServiceProvider.php
@@ -3,22 +3,38 @@ namespace BookManager\Providers;
 
 use Rabbit\Contracts\BootablePluginProviderInterface;
 use League\Container\ServiceProvider\AbstractServiceProvider;
+use BookManager\Services\BookPostTypeService;
 
 /**
  * Book Service Provider
  */
 class BookServiceProvider extends AbstractServiceProvider implements BootablePluginProviderInterface
 {
-    protected $provides = [];
+    protected $provides = [
+        'book.post_type'
+    ];
+    
     public function register()
     {
-        
+
+         $container = $this->getContainer();
+
+        // Register Book Post Type Service
+        $container->add('book.post_type', BookPostTypeService::class);
     }
     public function boot()
     {
-
+       
     }
     public function bootPlugin()
     {
+
+        $container = $this->getContainer();
+
+        // Initialize Book Post Type
+        add_action('init', function() use ($container) {
+            $postTypeService = $container->get('book.post_type');
+            $postTypeService->register();
+        });
     }
 }
