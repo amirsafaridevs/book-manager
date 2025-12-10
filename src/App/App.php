bd37f7d885ff3688676ae61dd45421417b0abb1a
Amir Safari <amir1.safari@gmail.com>
2025-12-10 17:15:31 +0330

    Add Persian translation and update text domain loading
    Added Persian (fa_IR) translation files and the main POT template for Book Manager. Refactored plugin text domain loading in App.php to use WordPress's load_plugin_textdomain on the plugins_loaded hook for proper localization support.

--- src/App/App.php
+++ src/App/App.php
@@ -139,11 +139,13 @@ class App extends Singleton
      */
     public function loadPluginTextDomain()
     {
-        $this->plugin->boot(
-            function( $plugin ) {
-                $plugin->loadPluginTextDomain();
-            }
-        );
+        add_action('plugins_loaded', function() {
+            load_plugin_textdomain(
+                'book-manager',
+                false,
+                dirname(plugin_basename($this->filePath)) . '/languages'
+            );
+        });
     }
 }
 
